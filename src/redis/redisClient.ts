e031497aba51016941845cc459ce1e23419f93ff
Josip Pokrajcic <wastelander2929@gmail.com>
2025-07-04 22:09:04 +0200

    Railway Redis config init change

--- src/redis/redisClient.ts
+++ src/redis/redisClient.ts
@@ -12,12 +12,14 @@ export function getRedisClient(): Redis {
     dotenv.config({ path: path.resolve(process.cwd(), envPath) });
 
     // Redis client setup
-    redisClient = new Redis({
-      host: process.env['REDIS_HOST'] || 'localhost',
-      port: parseInt(process.env['REDIS_PORT'] || '6379'),
-      password: process.env['REDIS_PASSWORD'] || '',
-      maxRetriesPerRequest: 3,
-    });
+    // redisClient = new Redis({
+    //   host: process.env['REDIS_HOST'] || 'localhost',
+    //   port: parseInt(process.env['REDIS_PORT'] || '6379'),
+    //   password: process.env['REDIS_PASSWORD'] || '',
+    //   maxRetriesPerRequest: 3,
+    // });
+
+    const redisClient = new Redis(process.env['REDIS_URL'] + '?family=0');
 
     // Handle Redis connection events
     redisClient.on('connect', () => {
